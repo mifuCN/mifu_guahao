89bacb649dac4fed4f755f93cc08734badd98bd8
mifuRD <1755786251@qq.com>
2022-11-25 12:43:04 +0800

    微调

--- fu-guahao-site/layouts/myfooter.vue
+++ fu-guahao-site/layouts/myfooter.vue
@@ -6,7 +6,7 @@
       <div class="right"><span
         class="v-link clickable" onclick="window.open('tencent://message/?uin=1755786251')"> 联系我们 </span><span
         class="v-link clickable" onclick="window.open('https://www.201314.tk/')"> 合作伙伴 </span><span
-        class="v-link clickable" onclick="window.open('https://201314.tk/project-training/')"> 其他项目 </span><span
+        class="v-link clickable" onclick="window.open('https://pan.201314.tk/')"> 其他项目 </span><span
         class="v-link clickable" onclick="window.open('https://201314.tk/%e9%9a%90%e7%a7%81%e6%94%bf%e7%ad%96/')"> 隐私协议 </span></div>
     </div>
   </div>
